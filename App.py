de23cb07cea4d236af46d3c3d948ec56ffa5a576
boedaka <oste.jannick@hotmail.com>
2020-07-28 23:12:24 +0200

    Update App.py

--- App.py
+++ App.py
@@ -5,13 +5,37 @@ class App(Engine):
     def __init__(self):
         """
             Todo list:
-                - Finish jumping method.
+                [Entitys]:
+                - Create interfaces for entitys
+                - Dedicated entity spawner.
+                [Player actions]:
+                - Check out jumping speed
+                - Add player freezing.
+                [Interface]
+                - Add interface system.
+                - Add message system.
+                [World]:
+                - Add objects spawner.
+                [Actions]:
+                - Add action handler
+                [Misc]:
                 - Improve function readability
                 - Add documentations
                 - Check why app.application.asset_folder disables load instead of loading custom folder.
-                - Create interfaces for entitys
-                - Dedicated entity spawner.
-                - better walking.
+                probably more...
+        """
+
+        """
+            Update list:
+                0.01 / 28/07/2020:
+                    [Project]:
+                    - Restructured files & converted to OOP.
+                    - Added Settings loader.
+                    
+                    [Player]:
+                    - Added Jumping
+                    - Added running
+                    
         """
         super(App, self).__init__(self)
 
